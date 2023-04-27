25061bebda0026e5aa1272d1a354b6b3c758a767
iZarrios <zarriose@gmail.com>
2023-04-28 00:11:48 +0200

    add: testing files

--- client.py
+++ client.py
@@ -2,7 +2,9 @@ import socket
 import pickle
 from dotenv import load_dotenv
 import os
+import time
 from tcp_packet import TCPPacket
+from udp_tcp_socket import TCPOverUDPSocket
 
 
 
@@ -17,10 +19,12 @@ ADDR=(address,port)
 
 
 # create a UDP socket
-udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+udp_socket = TCPOverUDPSocket()
 udp_socket.settimeout(timeout)
+udp_socket.connect(ADDR)
 
 while True:
     try:
@@ -31,12 +35,13 @@ while True:
 
         pkt.data = message
         pkt_pk= pickle.dumps(pkt)
-        udp_socket.sendto(pkt_pk,ADDR)
+        # udp_socket.sendto(pkt_pk,ADDR)
+        udp_socket.send(pkt_pk)
 
         # receive a response from the server
-        data, server_address = udp_socket.recvfrom(1024)
-        print('Received message:', data.decode())
-        print('From server:', server_address)
+        # data, server_address = udp_socket.recvfrom(1024)
+        # print('Received message:', data.decode())
+        # print('From server:', server_address)
     except socket.timeout:
         pass
 
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import socket
 from dotenv import load_dotenv
+import threading
 import os
-import pickle
 from tcp_packet import TCPPacket
+from udp_tcp_socket import TCPOverUDPSocket, pickle
 
 load_dotenv()
 port = int(os.getenv("PORT",8080))
@@ -13,32 +14,35 @@ timeout = int(os.getenv("TIMEOUT",1))
 ADDR=(address,port)
 
 # create a UDP socket
-udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+udp_socket = TCPOverUDPSocket()
 
 # bind the socket to a specific address and port
 udp_socket.bind(ADDR)
 udp_socket.settimeout(timeout)
 
+udp_socket.listen()
+
 print('Server is listening on', ADDR)
 
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        # recieve message from client
+        try:
+            data = conn.recvfrom(1024)
+            if not data:
+                break
+            print(pickle.loads(data[0]))
+        except socket.timeout:
+            print("Timeout")
+
 while True:
-    try:
-        # receive a message and the client address from the socket
-        data, client_address = udp_socket.recvfrom(1024)
-
-        pkt = TCPPacket.from_bytes(data)
-        print(pkt.seq,pkt.packet_type(),":",pkt.data)
-
-
-        
-        # print('Received message:', data)
-        print('From client:', client_address)
-
-        # send a response to the client
-        # response = 'Hello, client!'
-        # udp_socket.sendto(response.encode(), client_address)
-        # print('Sent response:', response)
-    except socket.timeout:
-        # print('Timeout')
+    addr, _= udp_socket.accept()
+    thread = threading.Thread(target=handle_client, args=(udp_socket, addr))
+    thread.start()
+    while udp_socket.status == 0:
         pass
