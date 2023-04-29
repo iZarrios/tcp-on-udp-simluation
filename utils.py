652bd2f0017947271fa9e078ab566abd9e82419c
iZarrios <zarriose@gmail.com>
2023-04-29 08:10:48 +0200

    delete: utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-import struct
-
-def pack_msg(struct_fmt="!I%ds", *args):
-    return struct.pack(struct_fmt, *args)
-
-def unpack_msg(struct_fmt="!I%ds", data=None):
-    return struct.unpack(struct_fmt, data)
